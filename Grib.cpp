da824beaad3cf53e80cdf28b40d4a5e8c0f09b43
Vanisch <staniszfabian@gmail.com>
2024-05-06 13:59:12 +0200

    1 commit

---
+++ Grib.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main()
+{
+	fstream grib;
+	grib.open("all.grib", ios::in | ios::binary);
+
+	if (!grib.is_open())
+	{
+		cout << "Nie udalo sie otworzyc pliku!\n";
+		return -1;
+	}
+
+	return 0;
+}
