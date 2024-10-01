279e8c70458696b0e48f0fc2dcdfa4666ff8840c
Vanisch <staniszfabian@gmail.com>
2024-10-01 16:45:41 +0200

    2 commit

--- Grib.cpp
+++ Grib.cpp
@@ -1,17 +1,25 @@
 ﻿#include <iostream>
 #include <fstream>
+#include <vector>
+#include <string>
 using namespace std;
 
 int main()
 {
-	fstream grib;
-	grib.open("all.grib", ios::in | ios::binary);
+    ifstream file("sample.grib", ios::binary);
+    if (!file) {
+        cout << "Error opening GRIB file.\n";
+        return 1;
+    }
 
-	if (!grib.is_open())
-	{
-		cout << "Nie udalo sie otworzyc pliku!\n";
-		return -1;
-	}
-
+    ofstream output("Output.txt");
+    if (!output) {
+        cout << "Error creating output file.\n";
+        return 1;
+ 	}
+    file.close();
+    output.close();
+    
+    cout << "GRIB info saved to Output.txt\n";
 	return 0;
 }
